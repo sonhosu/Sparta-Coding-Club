440c19081e4ee61a954a7e06e9342d4e3b890921
sonhosu <ghtn9398@nate.com>
2021-07-27 02:58:03 +0900

    관심상품 등록기능 추가

--- week04/src/main/resources/static/basic.js
+++ week04/src/main/resources/static/basic.js
@@ -104,8 +104,23 @@ function addProduct(itemDto) {
      * 1. contentType: "application/json",
      * 2. data: JSON.stringify(itemDto),
      */
+
     // 1. POST /api/products 에 관심 상품 생성 요청
+    $.ajax({
+        type: "POST",
+        url: "/api/products",
+        data: JSON.stringify(itemDto),
+        contentType:"application/json",
+        success:function (response){
+            $('#container').addClass('active');
+            targetId = response.id;
+
+        }
+
+    })
     // 2. 응답 함수에서 modal을 뜨게 하고, targetId 를 reponse.id 로 설정 (숙제로 myprice 설정하기 위함)
+
+
 }
 
 function showProduct() {
